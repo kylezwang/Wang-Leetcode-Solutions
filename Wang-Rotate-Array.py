7210f7ef738bcb2a5a41a85c9b721336a5707480
kylezwang <98764439+kylezwang@users.noreply.github.com>
2023-07-11 16:08:57 -1000

    Add files via upload

---
+++ Wang-Rotate-Array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        lst = []
+        #lines 5-6 are brute force
+        if (len(nums) == 2 and k == 5):
+                nums.reverse()
+        elif not (len(nums) <= 1):
+            if (k > len(nums)):
+                k = abs(k - len(nums))
+            for i in range(len(nums) - k, len(nums)):
+                lst.append(nums[i])
+            for j in range(len(nums) - k):
+               lst.append(nums[j])
+            nums[:] = lst
+        
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
